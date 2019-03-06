a01bb0f4ba4d515d72faaa787336a034e64198ae
Luiz Silva <luizauggusto@gmail.com>
2019-03-06 11:31:31 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Services;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -21,6 +22,32 @@ namespace SalesWebMvc.Controllers
             // Operação retorna uma lista contendo todos os Sellers do banco, recuperados pelo service SellerService
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost] // Anotation - informando post
+        [ValidateAntiForgeryToken] // Previne que sejam feitos ataques csrf, evita o envio de dados maliciosos durante a sessão de autenticação
+        public IActionResult Create(Seller seller )
+        {
+            _sellerService.Insert(seller); // Chama o método Insert do Service(SellerService)
+            // Retorna a tela Index, contendo os dados dos Sellers
+            return RedirectToAction(nameof(Index)); //Nameof, melhora a manutenção,não exige que seja feita alteração neste ponto, caso o titulo do método index seja alterado
+        }
+
+        // POST
+        [HttpPost] // Anotation - informando post
+        [ValidateAntiForgeryToken] // Previne que sejam feitos ataques csrf, evita o envio de dados maliciosos durante a sessão de autenticação
+        public IActionResult Delete(Seller seller)
+        {
+            _sellerService.Delete(seller); // Chama o método Delete do Service(SellerService)
+            // Retorna a tela Index, contendo os dados dos Sellers
+            return RedirectToAction(nameof(Index)); //Nameof, melhora a manutenção,não exige que seja feita alteração neste ponto, caso o titulo do método index seja alterado
         }
     }
 }
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -21,5 +21,23 @@ namespace SalesWebMvc.Services
             // Objeto _context (do tipo SalesWebMvcContext) acessa o banco, pegando dados da tabela Seller, e joga para uma lista
             return _context.Seller.ToList();
         }
+
+        public void Insert (Seller obj)
+        {
+            _context.Add(obj); // Insere o Seller
+            _context.SaveChanges(); // Insere e grava o novo Seller no banco de dados
+        }
+
+        public void Delete(Seller obj)
+        {
+            _context.Remove(obj); // Remove o Seller
+            _context.SaveChanges(); // Remove e grava o novo Seller no banco de dados
+        }
+
+        public void Update(Seller obj)
+        {
+            _context.Update(obj); // Atualiza o Seller
+            _context.SaveChanges(); // Atualiza e grava o novo Seller no banco de dados
+        }
     }
 }
